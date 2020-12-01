3e8403807d0665a2acf661446139ca687319a181
Ryan Hunt <roensb@gmail.com>
2020-11-30 22:30:37 -0800

    first commit
    day one code

---
+++ day 1 2020 advent.py
@@ -0,0 +1,22 @@
+
+def finder(values):
+    for value in values:
+        search = 2020 - value
+        newList = [item for item in values if item < search]
+        for newValue in newList:
+            search2 = 2020 - value - newValue
+            if search2 in newList:
+                print (newValue*value*search2)
+                return
+
+file = open("data.txt", "r")
+
+values = []
+
+for x in file :
+	values.append(int(x))
+
+
+
+
+finder(values)
