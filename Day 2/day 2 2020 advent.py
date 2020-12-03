b8bcf95d12be4a9993372a3daab195f73fa516a2
Ryan Hunt <roensb@gmail.com>
2020-12-02 22:02:52 -0800

    fixed stupid error

--- Day 2/day 2 2020 advent.py
+++ Day 2/day 2 2020 advent.py
@@ -8,13 +8,13 @@ file = open("day 2 input.txt", "r")
 count = 0
 count2 = 0
 for line in file :
-    line = line.strip(" ")
+    line = line.replace(" ", "")
     result = line.split(":")
     minVal,maxVal = result[0][:-1].split("-")
     letter = result[0][-1]
     if result[1].count(letter)<= int(maxVal) and result[1].count(letter)>= int(minVal):
         count = count + 1
-    if (result[1][int(maxVal)] == letter) ^ (result[1][int(minVal)] == letter):
+    if (result[1][int(maxVal)-1] == letter) ^ (result[1][int(minVal)-1] == letter):
         count2 = count2 + 1
 
 print ("P1:" + str(count))
