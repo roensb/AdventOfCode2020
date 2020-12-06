a09f7b4a2f4d65c553c44cd067f1aac5d741e724
Ryan Hunt <roensb@gmail.com>
2020-12-05 22:10:39 -0800

    cleaned up code

--- Day 6/day 5 2020 advent.py
+++ Day 6/day 5 2020 advent.py
@@ -1,23 +1,24 @@
+def countMatch(group):
+    total = 0
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    for char in letters:
+        matching = [s for s in group if char in s]
+        if len(matching) == len(group):
+            total +=1
+    return total
 
 file = open("data.txt", "r")
-
 group = []
 total = 0
 counts ={}
-letters = "abcdefghijklmnopqrstuvwxyz"
+
 for line in file:
     if line.strip() == "":
-        for char in letters:
-            matching = [s for s in group if char in s]
-            if len(matching) == len(group):
-                total +=1  
+        total += countMatch(group)
         group = []
     else :
         group.append(line.strip())
-for char in letters:
-    matching = [s for s in group if char in s]
-    if len(matching) == len(group):
-        total +=1  
+total += countMatch(group) 
 print ("Answer: " + str(total))
 
 
