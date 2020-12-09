17adbc0b9372e0afa9ed8e77782cd2818190849c
Ryan Hunt <roensb@gmail.com>
2020-12-08 23:37:54 -0800

    code clean up

--- Day 9/day 9 2020 advent ver2.py
+++ Day 9/day 9 2020 advent ver2.py
@@ -9,16 +9,14 @@ def valid(values, target):
 
 def search(values, found):
     window = []
-    total = 0
     for value in values:
-        if found == total:
+        if found == sum(window):
             result = min(window) + max(window)
             return result
         window.append(value)
-        total = sum(window)
-        while total > found:
+        while sum(window) > found:
             window.pop(0)
-            total = sum(window)
+
 
 file = open("data.txt", "r")
 values = []
@@ -29,7 +27,7 @@ for line in file:
 for i, value in enumerate(values):
     if i-preamble < 0 :
         continue
-    if not(Valid(values[i-preamble:i], value)):
+    if not(valid(values[i-preamble:i], value)):
         found = value
         break
 if found is None:
