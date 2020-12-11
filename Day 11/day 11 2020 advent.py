49fea0592e5fa3bcf2b4791d2efb9fbc18c14bf9
Ryan Hunt <roensb@gmail.com>
2020-12-10 23:15:23 -0800

    minor updates

--- Day 11/day 11 2020 advent.py
+++ Day 11/day 11 2020 advent.py
@@ -1,51 +1,58 @@
 
 def checkAdj(seats, row, col, seat):
-    N, S, E, W, NW, NE ,SE, SW = "","","","","","","",""
+    adjacent = []
+    #south
     for x in range(row+1, len(seats)):
         if not (seats[x][col] == "."):
-            S = seats[x][col]
+            adjacent.append(seats[x][col])
             break
+    #north
     for x in range(row-1, -1, -1):
         if not (seats[x][col] == "."):
-            N = seats[x][col]
+            adjacent.append(seats[x][col])
             break
+    #east
     for y in range(col+1, len(seats[0])):
         if not (seats[row][y] == "."):
-            E = seats[row][y]
+            adjacent.append(seats[row][y])
             break
+    #west
     for y in range(col-1, -1, -1):
         if not (seats[row][y] == "."):
-            W = seats[row][y]
+            adjacent.append(seats[row][y])
             break
+    #NE
     for i, x in enumerate(range(row-1, -1, -1)):
         y = col + i + 1
         if y >= len(seats[0]):
             break
         if not (seats[x][y] == "."):
-            NE = seats[x][y]
+            adjacent.append(seats[x][y])
             break
+    #NW
     for i, x in enumerate(range(row-1, -1, -1)):
         y = col - i - 1
         if y < 0:
             break
         if not (seats[x][y] == "."):
-            NW = seats[x][y]
+            adjacent.append(seats[x][y])
             break
+    #SW
     for i, x in enumerate(range(row+1, len(seats))):
         y = col - i - 1
         if y < 0:
             break
         if not (seats[x][y] == "."):
-            SW = seats[x][y]
+            adjacent.append(seats[x][y])
             break
+    #SE
     for i, x in enumerate(range(row+1, len(seats))):
         y = col + i + 1
         if y >= len(seats[0]):
             break
         if not (seats[x][y] == "."):
-            SE = seats[x][y]
+            adjacent.append(seats[x][y])
             break
-    adjacent = [N, S, E , W, NE, NW, SE, SW]
     if not("#" in adjacent) and seat == "L":
         return "#"
     if adjacent.count("#") >= 5 and seat == "#":
@@ -67,7 +74,6 @@ def occupy(seats):
 
     return seats
             
-
 file = open("data.txt", "r")
 seats =[]
 for line in file:
@@ -78,8 +84,3 @@ occupied = 0
 for row in finalSeats:
     occupied += row.count("#")
 print (occupied)
-
-
-
-
-
