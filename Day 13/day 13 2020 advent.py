25fecadd649843e3a00ac203ad6e079be8f11776
Ryan Hunt <roensb@gmail.com>
2020-12-12 23:38:34 -0800

    some clean up

--- Day 13/day 13 2020 advent.py
+++ Day 13/day 13 2020 advent.py
@@ -6,23 +6,18 @@ time = int(file[0].strip())
 buses = file[1].strip().split(",")
 busesInt = [int(x) for x in buses if x != "x"] 
 closest = 1
-first = 0
 remain = []
 for i, bus in enumerate(buses):
     if bus != "x":
       remain.append(int(bus)-i)
       
-print(crt(busesInt, remain))
-
-
 for value in busesInt:
     if math.ceil(time/int(value)) < math.ceil(time/closest):
         closest = int(value)
-
 bustime = closest*math.ceil(time/closest)
 wait = bustime - time
-
    
 print ("p1:" + str(wait*closest))
+print("p2:" +str(crt(busesInt, remain)[0]))
     
 
